19651ee6b9619fc285e48cd7d1bfe72aee8dd01b
daipn0906 <65522671+daipn0906@users.noreply.github.com>
2020-05-20 10:16:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>1st php auze</title>
+</head>
 <body>
 
+	<form action="">
 		Num a: <input type="text" name="a"> <br>
 		operator: <select name="op" id="">
 			<option value="1">+</option>
+			<option value="2">-</option>
 			<option value="3">*</option>
 			<option value="4">/</option>
+
 		</select><br>
 		Num b: <input type="text=" name="b"><br>
 		<input type="submit" value="calculate" name="cal">
